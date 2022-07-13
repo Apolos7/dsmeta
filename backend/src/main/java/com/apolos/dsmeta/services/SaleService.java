8cabf9ceaed4533ec02ed9b66e4bcf13c832cf2c
Apolos7 <jose.santana088@academico.ifs.edu.br>
2022-07-13 16:35:12 -0300

    Adiciona busca por data

--- backend/src/main/java/com/apolos/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/apolos/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.apolos.dsmeta.services;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.apolos.dsmeta.entities.Sale;
@@ -14,8 +19,15 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate minDateParse = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate maxDateParse = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return saleRepository.findSales(minDateParse, maxDateParse, pageable);
 	}
 	
 }
