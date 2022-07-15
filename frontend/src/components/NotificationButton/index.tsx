a536a5a6a03b5d36a925e8dd4498b8352c4bd2ad
Apolos7 <jose.santana088@academico.ifs.edu.br>
2022-07-15 12:57:55 -0300

    Adiciona a funcionalidade de envio de SMS

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,29 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function NotificationButton({saleId}: Props) {
     return (
         <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notification button"/>
+            <img src={icon} alt="Notification button" onClick={() => handleClick(saleId)}/>
         </div>
     )
 }
 
+function handleClick(saleId: number): void {
+    console.log(`Enviando requisição do usuário ${saleId}`)
+    axios(`${BASE_URL}/sales/${saleId}/notification`)
+        .then(response => {
+            toast.info("SMS enviado com sucesso")
+        })
+}
+
 export default NotificationButton;
+
+
